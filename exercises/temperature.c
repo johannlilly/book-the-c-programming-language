31fbee01a836793be61be1de873956b6356e1931
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-02-12 18:28:33 -0600

    Use symbolic constants: offload some variables to preprocessor using #define

--- exercises/temperature.c
+++ exercises/temperature.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
+#define LOWER	0	/* lower limit of table */
+#define UPPER	300	/* upper limit */
+#define STEP	20	/* step size */
+
 main() {
 
 	float fahr, celsius;
-	int lower, upper, step;
-
-	lower = 0;
-	upper = 300;
-	step = 20;
 	
 	fahr = (float) lower;
+
 	while (fahr <= (float) upper) {
 		celsius = 5.0 * (fahr-32.0) / 9.0;
 		printf("%3.0f %6.1f\n", fahr, celsius);
