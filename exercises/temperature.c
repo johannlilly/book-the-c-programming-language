ac01c514d7d55f4608d8f9060f30f9b1f9eb8377
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-02-12 18:09:54 -0600

    specify type cast

--- exercises/temperature.c
+++ exercises/temperature.c
@@ -10,7 +10,7 @@ main() {
 	step = 20;
 	
 	fahr = (float) lower;
-	while (fahr <= upper) {
+	while (fahr <= (float) upper) {
 		celsius = 5.0 * (fahr-32.0) / 9.0;
 		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
